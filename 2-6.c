c3666eab710f66547b5cd1c8f9245f7f3c5be5a3
Enzo Barrett <enzobarrett@gmail.com>
2019-06-06 21:59:28 -0600

    Add 2-6 with setbits(x, p, n, y)

---
+++ 2-6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/*
+ * define setbits(x, p, n, y);
+ *
+ * returns x with the n bits that begin at position p set to the rightmost n
+ * bits of y, leaving the other bits unchanged.
+ *
+ * © Enzo Barrett 2019
+ *
+ */
+
+void printBits(unsigned int num);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
+
+main()
+{
+    int first = 74;
+    printf("%d", setbits(152, 4, 3, 229));
+}
+
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+    unsigned xMask = ~((~0 << (p+1)) ^ (~0 << (p+1-n)));
+    unsigned yMask = ~(~0 << n);
+    unsigned xComb = x & xMask;
+    unsigned yComb = y & yMask;
+    yComb <<= p + 1 - n;
+    return xComb | yComb;
+}
